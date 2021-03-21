449133cca28665f857daac4714da8f2f3abffb7b
Erik-GT-97 <erik.fisi16@gmail.com>
2021-03-21 17:42:13 -0500

    first commit

---
+++ snowpack.config.js
@@ -0,0 +1,12 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  buildOptions: {
+    baseUrl: '',
+    out:'docs',
+    metaUrlPath:'snowpack'
+  },
+}
